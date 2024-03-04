cbc5ad505408ae471709f694be091b0c0c097189
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-04 03:57:28 +0200

    add brn color audio tiles cont

--- simon-says-game/js/main.js
+++ simon-says-game/js/main.js
@@ -1,5 +1,5 @@
-const btn = document.getElementById("play")
-const randomColors = ["red", "green", "yellow", ""]
-btn.addEventListener("click", function(){
-    console.log("clicked")
-})
+const btn = document.getElementById("play");
+const colors = ["blue", "red", "yellow", "green"];
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const tiles = document.querySelectorAll('.tile');
+const container = document.querySelector(".container");
