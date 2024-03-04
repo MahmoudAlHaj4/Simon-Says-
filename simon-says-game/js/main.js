46a977d9b7183b413b268351c98f2d87c3d2b4bc
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-04 03:58:26 +0200

    add event listerner

--- simon-says-game/js/main.js
+++ simon-says-game/js/main.js
@@ -3,3 +3,22 @@ const colors = ["blue", "red", "yellow", "green"];
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const tiles = document.querySelectorAll('.tile');
 const container = document.querySelector(".container");
+
+
+btn.addEventListener("click", function () {
+    const color = colors[generatePattern()];
+    console.log("color:", color);
+
+    playSound(color);
+
+    container?.classList.remove("unclickable");
+    
+    tiles.forEach((tile, index) => {
+        setTimeout(() => {
+            tile.classList.remove("inactive");
+            setTimeout(() => {
+                tile.classList.add("inactive");
+            }, 500);
+        }, index * 1000);
+    });
+});
